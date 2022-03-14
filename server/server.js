daceed0f46ba73224783071a38685356cc2c6cee
acdodd17 <acdodd17@gmail.com>
2022-03-14 11:58:01 -0400

    still not working

--- server/server.js
+++ server/server.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schemas");
 
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
-
-// const routes = require('./routes');
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
-  const server = new ApolloServer({ 
-    typeDefs, 
+  const server = new ApolloServer({
+    typeDefs,
     resolvers,
-    context: authMiddleware 
+    introspection: process.env.NODE_ENV !== 'production',
+    context: authMiddleware,
   });
 
   // Start the Apollo server
@@ -29,24 +26,20 @@ const startServer = async () => {
   // log where we can go to test our GQL API
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
-
-// Initialize the Apollo server
 startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// If in production mode ...
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
-// app.use(routes);
-
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
